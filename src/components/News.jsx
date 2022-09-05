23a14b7c373538d3dc5d5c180006f7d05c0fbf77
RonitGandhi <ronitgandhi96@gmail.com>
2022-09-05 20:48:41 +0530

    minor

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -13,13 +13,36 @@ export class News extends Component {
 		};
 	}
 	async componentDidMount() {
-		let url =
-			"https://newsapi.org/v2/top-headlines?country=in&apiKey=a6ae65bc33dc45999b120f99174a62a1";
+		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a6ae65bc33dc45999b120f99174a62a1&page=1&pageSize=20`;
 		let data = await fetch(url);
 		let parseData = await data.json();
 		// console.log(parseData);
 		this.setState((this.articles = parseData.articles));
 	}
+	handleNextButton = async () => {
+		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a6ae65bc33dc45999b120f99174a62a1&pageSize=20&page=${
+			this.state.page + 1
+		}`;
+		let data = await fetch(url);
+		let parseData = await data.json();
+		// console.log(parseData);
+		this.setState((this.articles = parseData.articles));
+		this.setState({
+			page: this.state.page + 1,
+		});
+	};
+	handlePreviousButton = async () => {
+		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a6ae65bc33dc45999b120f99174a62a1&pageSize=20&page=${
+			this.state.page - 1
+		}`;
+		let data = await fetch(url);
+		let parseData = await data.json();
+		// console.log(parseData);
+		this.setState((this.articles = parseData.articles));
+		this.setState({
+			page: this.state.page - 1,
+		});
+	};
 
 	render() {
 		return (
@@ -39,12 +62,21 @@ export class News extends Component {
 						);
 					})}
 				</div>
-				<div className="container">
-					<button type="button" className="btn btn-info">
-						Previous
+				<div className="container d-flex justify-content-between">
+					<button
+						onClick={this.handlePreviousButton}
+						type="button"
+						className="btn btn-info"
+						disabled={this.state.page <= 1}
+					>
+						&larr; Previous
 					</button>
-					<button type="button" className="btn btn-info">
-						Next
+					<button
+						onClick={this.handleNextButton}
+						type="button"
+						className="btn btn-info"
+					>
+						Next &rarr;
 					</button>
 				</div>
 			</div>
