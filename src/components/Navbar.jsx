8f2d223b446db4eb184a75622cf6e57b4a3ab01b
RonitGandhi <ronitgandhi96@gmail.com>
2022-09-07 15:00:47 +0530

    added react-router-dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class Navbar extends Component {
+	navbar = [
+		{
+			id: 2,
+			link: "Business",
+		},
+		{
+			id: 3,
+			link: "Entertainment",
+		},
+
+		{
+			id: 5,
+			link: "Health",
+		},
+		{
+			id: 6,
+			link: "Science",
+		},
+		{
+			id: 7,
+			link: "Sports",
+		},
+		{
+			id: 9,
+			link: "technology",
+		},
+	];
 	render() {
 		return (
 			<div>
-				<nav className="navbar navbar-expand-lg bg-light">
+				<nav className={`navbar navbar-expand-lg bg-light`}>
 					<div className="container-fluid">
-						<a className="navbar-brand" href="/">
+						<Link className="navbar-brand" to="/">
 							NewsApp
-						</a>
+						</Link>
 						<button
 							className="navbar-toggler"
 							type="button"
@@ -25,20 +53,17 @@ export class Navbar extends Component {
 							id="navbarSupportedContent"
 						>
 							<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-								<li className="nav-item">
-									<a
-										className="nav-link active"
-										aria-current="page"
-										href="/"
-									>
-										Home
-									</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="/about">
-										Link
-									</a>
-								</li>
+								{this.navbar.map(({ id, link }) => (
+									<li key={id} className="nav-item">
+										<Link
+											className="nav-link"
+											aria-current="page"
+											to={`${link}`}
+										>
+											{link}
+										</Link>
+									</li>
+								))}
 							</ul>
 						</div>
 					</div>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 
 export class News extends Component {
+	static defaultProps = {
+		countryName: "in",
+		page_size: 5,
+		category: "general",
+	};
+
+	static propTypes = {
+		countryName: PropTypes.string,
+		page_size: PropTypes.number,
+		category: PropTypes.string,
+	};
+
 	articles = [];
+
 	constructor() {
 		super();
 
@@ -13,56 +28,80 @@ export class News extends Component {
 		};
 	}
 	async componentDidMount() {
-		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a6ae65bc33dc45999b120f99174a62a1&page=1&pageSize=20`;
-		let data = await fetch(url);
-		let parseData = await data.json();
-		// console.log(parseData);
-		this.setState((this.articles = parseData.articles));
-	}
-	handleNextButton = async () => {
-		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a6ae65bc33dc45999b120f99174a62a1&pageSize=20&page=${
-			this.state.page + 1
-		}`;
+		let url = `https://newsapi.org/v2/top-headlines?country=${this.props.countryName}&category=${this.props.category}&apiKey=a6ae65bc33dc45999b120f99174a62a1&page=1&pageSize=${this.props.page_size}`;
+		this.setState({ loading: true });
 		let data = await fetch(url);
 		let parseData = await data.json();
 		// console.log(parseData);
 		this.setState((this.articles = parseData.articles));
 		this.setState({
+			loading: false,
+		});
+	}
+	handleNextButton = async () => {
+		let url = `https://newsapi.org/v2/top-headlines?country=${
+			this.props.countryName
+		}&category=${
+			this.props.category
+		}&apiKey=a6ae65bc33dc45999b120f99174a62a1&pageSize=${
+			this.props.page_size
+		}&page=${this.state.page + 1}`;
+		this.setState({ loading: true });
+		let data = await fetch(url);
+		let parseData = await data.json();
+		// console.log(parseData);
+		this.setState((this.articles = parseData.articles));
+
+		this.setState({
 			page: this.state.page + 1,
+			loading: false,
 		});
 	};
 	handlePreviousButton = async () => {
-		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a6ae65bc33dc45999b120f99174a62a1&pageSize=20&page=${
-			this.state.page - 1
-		}`;
+		let url = `https://newsapi.org/v2/top-headlines?country=${
+			this.props.countryName
+		}&category=${
+			this.props.category
+		}&apiKey=a6ae65bc33dc45999b120f99174a62a1&pageSize=${
+			this.props.page_size
+		}&page=${this.state.page - 1}`;
+		this.setState({ loading: true });
 		let data = await fetch(url);
 		let parseData = await data.json();
 		// console.log(parseData);
 		this.setState((this.articles = parseData.articles));
 		this.setState({
 			page: this.state.page - 1,
+			loading: false,
 		});
 	};
 
 	render() {
 		return (
 			<div className="container my-3">
-				<h1>Top Head Lines</h1>
+				<h1 className="text-center" style={{ margin: "35px" }}>
+					Top Head Lines
+				</h1>
+				{this.state.loading && <Spinner />}
 				<div className="row">
-					{this.articles.map((element) => {
-						return (
-							<div className="col-md-3" key={element.url}>
-								<NewsItem
-									title={element.title}
-									description={element.description}
-									imageUrl={element.urlToImage}
-									newsUrl={element.url}
-								/>
-							</div>
-						);
-					})}
+					{!this.state.loading &&
+						this.articles.map((element) => {
+							return (
+								<div className="col-md-3" key={element.url}>
+									<NewsItem
+										title={element.title}
+										description={element.description}
+										imageUrl={element.urlToImage}
+										newsUrl={element.url}
+									/>
+								</div>
+							);
+						})}
 				</div>
-				<div className="container d-flex justify-content-between">
+				<div
+					className="container d-flex justify-content-between"
+					style={{ margin: "35px" }}
+				>
 					<button
 						onClick={this.handlePreviousButton}
 						type="button"
